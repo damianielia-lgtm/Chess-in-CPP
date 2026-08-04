dbc9548d69ba42bf8266ca4e372e70c9a11da84f
Elia <damianielia@yahoo.com>
2026-08-05 00:06:01 +0300

    Better board printing

--- src/interface/display.cpp
+++ src/interface/display.cpp
@@ -2,24 +2,33 @@
 
 #include <iostream>
 #include <array>
-#include <string>
+#include <string_view>
 
 #include "../core/position.h"
 #include "../core/square.h"
 
+constexpr std::string_view sperator = "  +---+---+---+---+---+---+---+---+  \n";
+constexpr std::string_view files = "    a   b   c   d   e   f   g   h    \n";
+constexpr std::string_view flipped_files = "    h   g   f   e   d   c   b   a    \n";
+
 void print_board(const Position& position, const bool flip) {
-    std::string files = flip ? "    h g f e d c b a    \n" : "    a b c d e f g h    \n";
+    std::string_view files = flip ? files : flipped_files;
+
     std::cout << files;
-    std::cout << "   -----------------   \n";
+    std::cout << sperator;
+
     for (int rank = 7; rank >= 0; --rank) {
         int display_rank = flip ? 7 - rank : rank;
         std::cout << display_rank + 1 << " | ";
+
         for (int file = 0; file <= 7; file++) {
             int display_file = flip ? 7 - file : file;
-            std::cout << position.piece_at(Square(display_rank * 8 + display_file)).symbol() << ' ';
+            std::cout << position.piece_at(Square(display_rank * 8 + display_file)).symbol() << " | ";
         }
-        std::cout << "| " << display_rank + 1 << "\n";
+
+        std::cout << display_rank + 1 << '\n';
+        std::cout << sperator;
     }
-    std::cout << "   -----------------   \n";
+    
     std::cout << files;
 }
